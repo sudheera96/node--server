2d6ba87e9f3ebd83358b3fd5b72f0e3945482201
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-03-24 23:11:19 -0500

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require('http');
-const PORT = process.env.PORT || 5000;
+
+const hostname = process.env.hostname || '192.168.0.21';
+const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello,This is Sri Sudheera Chitipolu.');
 });
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// const http = require('http');
+// const PORT = process.env.PORT || 5000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+// server.listen(PORT, () => {
+//   console.log(`Server running on ${PORT}/`);
+// });
