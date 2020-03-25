e4c3d8b800ceef49a6e9735b8b6fb1c6e85cd711
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-03-24 23:20:09 -0500

    Updated

--- index.js
+++ index.js
@@ -12,14 +12,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// const http = require('http');
-// const PORT = process.env.PORT || 5000;
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-// server.listen(PORT, () => {
-//   console.log(`Server running on ${PORT}/`);
-// });
