ead3e91bff4a0458d9f233742d260d51dddd1fd3
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-03-24 22:51:32 -0500

    Updated

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const http = require('http');
+const http = require('http')  // require built-in Node.js http package
 
-//const hostname = process.env.hostname || '192.168.0.21';
-const port = process.env.PORT || 3000;
+// Use hosting values if available, otherwise default 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
 
+// define our server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello,This is Sri Sudheera Chitipolu.');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World! This is text - we can respond with HTML, JSON, and more :)\n')
+})
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
